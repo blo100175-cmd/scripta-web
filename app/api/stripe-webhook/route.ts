d21a0be516b7003de225f50ec05a84e259203893
blo100175 <blo100175@gmail.com>
2026-03-18 20:43:40 +0800

    Fix: supabase env type + webhook fix

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -4,10 +4,15 @@ import { createClient } from "@supabase/supabase-js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-const supabase = createClient(
+/*const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+);*/
+
+const supabase = createClient(                  //|----- 🟡🟡 PATCHED 18/3/26
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);                                        //-----|🟡🟡 18/3/26
 
 function getCurrentMonthKey() {
   const now = new Date();
